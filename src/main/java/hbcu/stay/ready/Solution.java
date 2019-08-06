789cee42cf797a7c779f0adca9b9b6c73218c18e
Joshua <jds13245@hotmail.com>
2019-08-06 14:02:04 -0400

    Successfully passed all test cases.

--- src/main/java/hbcu/stay/ready/Solution.java
+++ src/main/java/hbcu/stay/ready/Solution.java
@@ -1,9 +1,36 @@
 package hbcu.stay.ready;
 
+import java.util.HashMap;
+
 public class Solution {
 
     public String isPalindrome(String input){
-        return null;
+        HashMap<Character, Integer> letterTracker = new HashMap<Character, Integer>();
+
+        char[] charredInput = input.toCharArray();
+        for (char c : charredInput) {
+            if(!letterTracker.containsKey(c)) {
+                letterTracker.put(c, 1);
+            }
+            else {
+                int currentValue = letterTracker.get(c);
+                currentValue++;
+                letterTracker.put(c,currentValue);
+            }
+        }
+
+        int limit = 0;
+        for (int values : letterTracker.values()) {
+            if(values%2 == 1) {
+                limit++;
+            }
+        }
+
+        if (limit > 1) {
+            return "NO";
+        }
+
+        return "YES";
     }
 
 }
